ed4aad7b028512440799ca1beee829b799efed0c
Raul-Gonzalez <raul.gonzalez@sjsu.edu>
2016-07-04 16:34:22 -0700

    Signed-off-by: Raul-Gonzalez <raul.gonzalez@sjsu.edu>

---
+++ Homework/Assignment3/Gaddis_7thEd_Chap4_Prob6_MassWeight/main.cpp
@@ -0,0 +1,41 @@
+/* 
+ * File:   main.cpp
+ * Author: Raul Gonzalez
+ * Created on June 27, 2016, 9:03 PM
+ * Purpose:  Template
+ */
+
+//System Libraries
+#include <iostream>  //Input/Output Library
+#include <iomanip>   //Manipulate Data
+using namespace std; //Namespace of the System Libraries
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+    float massKg, massNewt, gravity=9.8;
+    
+    //Input Data
+    cout<<"Enter the objects mass"<<endl;
+    cin>>massKg;
+    
+    //Process the Data
+    massNewt=massKg*gravity;
+    
+    
+    //Output the processed Data
+    cout<<fixed<<setprecision(2)<<endl;
+    cout<<"The mass in newtons is "<<massNewt<<endl;
+    if(massNewt>=1000)
+        cout<<"The object is too heavy"<<endl;
+    else cout<<"The object is too light"<<endl;
+    
+    //Exit Stage Right!
+    return 0;
+}
